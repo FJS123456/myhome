ec457604bc6ddca7213d687c9707e55fcedabc95
FJS123456 <fujisheng@fujishengdemacbook-air.local>
2016-03-14 09:05:14 +0800

    Initial Commit

---
+++ Home/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Home
+//
+//  Created by fujisheng on 16/3/14.
+//  Copyright © 2016年 fujisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Home/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Home
+//
+//  Created by fujisheng on 16/3/14.
+//  Copyright © 2016年 fujisheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
